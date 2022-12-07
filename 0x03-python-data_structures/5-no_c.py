3951326795468a65ddfbf2713f0d2096a7d22d0a
Mwenje001 <mwenje72@gmail.com>
2022-12-07 08:37:58 +0300

    may work

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new_str += i
-    return (new_str)
-# return my_string.translate({ord(c): None for c in "cC"})
+    string_length = len(my_string)
+
+    i = 0
+    new_string = my_string[:]
+
+    for char in range(string_length):
+        if (my_string[char] == 'c' or my_string[char] == 'C'):
+            new_string = new_string[:(char - i)] + my_string[(char + 1):]
+            i += 1
+
+    return (new_string)
